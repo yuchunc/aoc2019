ee5874892d72e66012220ea285b82bf5ed190bbe
Mickey Chen <hey.mickabo@gmail.com>
2019-12-01 14:47:00 +0800

    init commit

---
+++ lib/aoc.ex
@@ -0,0 +1,18 @@
+defmodule Aoc do
+  @moduledoc """
+  Documentation for Aoc2019.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2019.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
